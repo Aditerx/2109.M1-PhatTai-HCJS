64d7a8d2d4f31e1bc3c9ee9d3f60da06b9023f8d
Aditerx <phattai3178@gmail.com>
2021-11-23 09:13:55 +0700

    Upload Nov 23, 2021

---
+++ Nguyen Phat Tai 2109.M1/js.js
@@ -0,0 +1,29 @@
+function checkLogin(){
+    var first = formLOG.txtFirst.value;
+    var last = formLOG.txtLast.value;
+    var email = formLOG.txtMail.value;
+    var tel = formLOG.phone.value;
+    if(first == ""){
+        alert("First name must not be blank!");
+        return false;
+    }
+
+    if(last == ""){
+        alert("Last name must not be blank!");
+        return false;
+    }
+
+    if(email == ""){
+        alert("Email must not be blank!");
+        return false;
+    }
+
+    if(teL == ""){
+        alert("Telephone must not be blank!");
+        return false;
+    }
+
+    else{
+        alert("Congratulation! Your information has been sent successfully!");
+    }
+}
